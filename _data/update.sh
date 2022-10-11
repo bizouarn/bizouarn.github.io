113536cf80b059a7964c87e26fc8a5e2de0f1481
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2022-10-11 09:41:29 +0000

    🗃 Update data

--- _data/update.sh
+++ _data/update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget 'https://api.github.com/users/bizouarn/repos' -O repo.json
 wget 'https://api.github.com/users/bizouarn/gists' -O gist.json
+git add .
+git commit -m "🗃 Update data"
+git push
