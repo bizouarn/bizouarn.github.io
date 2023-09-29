4563b1cf49d05275793e9610552eb6b374a5df53
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2023-09-29 22:59:23 +0200

    🧛‍♂️ Add dark theme

---
+++ CDN/sign.js
@@ -0,0 +1,8 @@
+var signarture = "\
+--------------------------\n\
+(\\_/)\n\
+(°_°)\n\
+/ > Aymeric Bizouarn "+new Date().getFullYear()+" ©\n\
+--------------------------\n\
+"
+console.log(signarture)
