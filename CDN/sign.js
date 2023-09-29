e31011486dd0df70a7d0e10c6ce72c1f19fa615f
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2023-09-29 19:54:44 +0200

    📦 Update CDN libs

--- CDN/sign.js
+++
@@ -1,8 +0,0 @@
-var signarture = "\
---------------------------\n\
-(\\_/)\n\
-(°_°)\n\
-/ > Aymeric Bizouarn "+new Date().getFullYear()+" ©\n\
---------------------------\n\
-"
-console.log(signarture)
