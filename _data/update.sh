f8baa2b1972817acc9ad14e2ed65011457862d14
bizouarn <aymeric.bizouarn@hotmail.fr>
2023-12-21 16:32:41 +0100

    🗃 Update data

--- _data/update.sh
+++ _data/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget 'https://api.github.com/users/bizouarn/repos' -O repo.json
-wget 'https://api.github.com/users/bizouarn/gists' -O gist.json
+wget 'https://api.github.com/users/bizouarn/repos' -O 'repo.json'
+wget 'https://api.github.com/users/bizouarn/gists' -O 'gist.json'
 git add .
 git commit -m "🗃 Update data"
 git push
