fe28d61f1b9e5d82c19ef45c40cff191f07202a8
Aymeric BIZOUARN <a.bizouarn@herakles.com>
2025-04-08 08:12:35 +0200

    🐛 Increases the number of notes displayed

--- _data/update.sh
+++ _data/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget 'https://api.github.com/users/bizouarn/repos' -O 'repo.json'
-wget 'https://api.github.com/users/bizouarn/gists' -O 'gist.json'
+wget 'https://api.github.com/users/bizouarn/repos?per_page=100' -O 'repo.json'
+wget 'https://api.github.com/users/bizouarn/gists?per_page=100' -O 'gist.json'
 git add .
 git commit -m "🗃 Update data"
 git push
